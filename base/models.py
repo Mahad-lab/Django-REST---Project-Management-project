9922750a23493a2d80e0ba7fef94ab21f3650ef1
Mahad <56235065+Mahad-lab@users.noreply.github.com>
2024-06-19 00:53:51 +0000

    🚀 Add `Project` & `Tasks` models, serializers, views & URLs

--- base/models.py
+++ base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    deleted_at = models.DateTimeField(null=True)
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=50)
+    due_date = models.DateField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    deleted_at = models.DateTimeField(null=True)
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
--- base/views.py
+++ base/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project, Task
+from .serializers import ProjectSerializer, TaskSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.filter(deleted_at__isnull=True)
+    serializer_class = ProjectSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.filter(deleted_at__isnull=True)
+    serializer_class = TaskSerializer
